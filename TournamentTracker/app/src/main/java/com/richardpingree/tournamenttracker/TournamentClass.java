094d74f8a7fb8f107e2945e9ac53ace245d1cfa5
richpingree <richpingree@fullsail.edu>
2015-05-12 21:32:15 -0500

    created and coded Tournament Class
    created and coded Tournament Class

---
+++ TournamentTracker/app/src/main/java/com/richardpingree/tournamenttracker/TournamentClass.java
@@ -0,0 +1,340 @@
+package com.richardpingree.tournamenttracker;
+
+import java.io.Serializable;
+
+/**
+ * Created by Richard Pingree APD2 1505 on 5/12/15.
+ */
+public class TournamentClass implements Serializable {
+
+    public String tournyName;
+    public String p1Name;
+    public int p1Payout;
+    public int p1Points;
+    public String p2Name;
+    public int p2Payout;
+    public int p2Points;
+    public String p3Name;
+    public int p3Payout;
+    public int p3Points;
+    public String p4Name;
+    public int p4Payout;
+    public int p4Points;
+    public String p5Name;
+    public int p5Payout;
+    public int p5Points;
+    public String p6Name;
+    public int p6Payout;
+    public int p6Points;
+    public String p7Name;
+    public int p7Payout;
+    public int p7Points;
+    public String p8Name;
+    public int p8Payout;
+    public int p8Points;
+    public String p9Name;
+    public int p9Payout;
+    public int p9Points;
+    public String p10Name;
+    public int p10Payout;
+    public int p10Points;
+
+    public TournamentClass(){
+
+    }
+
+    public TournamentClass(String tournamentName, String player1Name, int player1Payout, int player1Points,
+                           String player2Name, int player2Payout, int player2Points,
+                           String player3Name, int player3Payout, int player3Points,
+                           String player4Name, int player4Payout, int player4Points,
+                           String player5Name, int player5Payout, int player5Points,
+                           String player6Name, int player6Payout, int player6Points,
+                           String player7Name, int player7Payout, int player7Points,
+                           String player8Name, int player8Payout, int player8Points,
+                           String player9Name, int player9Payout, int player9Points,
+                           String player10Name, int player10Payout, int player10Points){
+        p1Name = player1Name;
+        p1Payout = player1Payout;
+        p1Points = player1Points;
+        p2Name = player2Name;
+        p2Payout = player2Payout;
+        p2Points = player2Points;
+        p3Name = player3Name;
+        p3Payout = player3Payout;
+        p3Points = player3Points;
+        p4Name = player4Name;
+        p4Payout = player4Payout;
+        p4Points = player4Points;
+        p5Name = player5Name;
+        p5Payout = player5Payout;
+        p5Points = player5Points;
+        p6Name = player6Name;
+        p6Payout = player6Payout;
+        p6Points = player6Points;
+        p7Name = player7Name;
+        p7Payout = player7Payout;
+        p7Points = player7Points;
+        p8Name = player8Name;
+        p8Payout = player8Payout;
+        p8Points = player8Points;
+        p9Name = player9Name;
+        p9Payout = player9Payout;
+        p9Points = player9Points;
+        p10Name = player10Name;
+        p10Payout = player10Payout;
+        p10Points = player10Points;
+    }
+
+    public String getTournyName(){
+        return tournyName;
+    }
+
+    public void setTournyName(){
+        this.tournyName = tournyName;
+    }
+
+    public String getP1Name(){
+        return p1Name;
+    }
+
+    public void setP1Name(){
+        this.p1Name = p1Name;
+    }
+
+    public int getP1Payout(){
+        return p1Payout;
+    }
+
+    public void setP1Payout(){
+        this.p1Payout = p1Payout;
+    }
+
+    public int getP1Points(){
+        return p1Points;
+    }
+
+    public void setP1Points(){
+        this.p1Points = p1Points;
+    }
+
+    public String getP2Name(){
+        return p2Name;
+    }
+
+    public void setP2Name(){
+        this.p2Name = p2Name;
+    }
+
+    public int getP12Payout(){
+        return p2Payout;
+    }
+
+    public void setP2Payout(){
+        this.p2Payout = p2Payout;
+    }
+
+    public int getP2Points(){
+        return p2Points;
+    }
+
+    public void setP2Points(){
+        this.p2Points = p2Points;
+    }
+
+    public String getP3Name(){
+        return p3Name;
+    }
+
+    public void setP3Name(){
+        this.p3Name = p3Name;
+    }
+
+    public int getP3Payout(){
+        return p3Payout;
+    }
+
+    public void setP3Payout(){
+        this.p3Payout = p3Payout;
+    }
+
+    public int getP3Points(){
+        return p3Points;
+    }
+
+    public void setP3Points(){
+        this.p3Points = p3Points;
+    }
+
+    public String getP4Name(){
+        return p4Name;
+    }
+
+    public void setP4Name(){
+        this.p4Name = p4Name;
+    }
+
+    public int getP4Payout(){
+        return p4Payout;
+    }
+
+    public void setP4Payout(){
+        this.p4Payout = p4Payout;
+    }
+
+    public int getP4Points(){
+        return p4Points;
+    }
+
+    public void setP4Points(){
+        this.p4Points = p4Points;
+    }
+
+    public String getP5Name(){
+        return p5Name;
+    }
+
+    public void setP5Name(){
+        this.p5Name = p5Name;
+    }
+
+    public int getP5Payout(){
+        return p5Payout;
+    }
+
+    public void setP5Payout(){
+        this.p5Payout = p5Payout;
+    }
+
+    public int getP5Points(){
+        return p5Points;
+    }
+
+    public void setP5Points(){
+        this.p5Points = p5Points;
+    }
+
+    public String getP6Name(){
+        return p6Name;
+    }
+
+    public void setP6Name(){
+        this.p6Name = p6Name;
+    }
+
+    public int getP6Payout(){
+        return p6Payout;
+    }
+
+    public void setP6Payout(){
+        this.p6Payout = p6Payout;
+    }
+
+    public int getP6Points(){
+        return p6Points;
+    }
+
+    public void setP6Points(){
+        this.p6Points = p6Points;
+    }
+
+    public String getP7Name(){
+        return p7Name;
+    }
+
+    public void setP7Name(){
+        this.p7Name = p7Name;
+    }
+
+    public int getP7Payout(){
+        return p7Payout;
+    }
+
+    public void setP7Payout(){
+        this.p7Payout = p7Payout;
+    }
+
+    public int getP7Points(){
+        return p7Points;
+    }
+
+    public void setP7Points(){
+        this.p7Points = p7Points;
+    }
+
+    public String getP8Name(){
+        return p8Name;
+    }
+
+    public void setP8Name(){
+        this.p8Name = p8Name;
+    }
+
+    public int getP8Payout(){
+        return p8Payout;
+    }
+
+    public void setP8Payout(){
+        this.p8Payout = p8Payout;
+    }
+
+    public int getP8Points(){
+        return p8Points;
+    }
+
+    public void setP8Points(){
+        this.p8Points = p8Points;
+    }
+
+    public String getP9Name(){
+        return p9Name;
+    }
+
+    public void setP9Name(){
+        this.p9Name = p9Name;
+    }
+
+    public int getP9Payout(){
+        return p9Payout;
+    }
+
+    public void setP9Payout(){
+        this.p9Payout = p9Payout;
+    }
+
+    public int getP9Points(){
+        return p9Points;
+    }
+
+    public void setP9Points(){
+        this.p9Points = p9Points;
+    }
+
+    public String getP10Name(){
+        return p10Name;
+    }
+
+    public void setP10Name(){
+        this.p10Name = p10Name;
+    }
+
+    public int getP10Payout(){
+        return p10Payout;
+    }
+
+    public void setP10Payout(){
+        this.p10Payout = p10Payout;
+    }
+
+    public int getP10Points(){
+        return p10Points;
+    }
+
+    public void setP10Points(){
+        this.p10Points = p10Points;
+    }
+
+    @Override
+    public String toString() {
+        return tournyName;
+    }
+}
---
+++ TournamentTracker/app/src/main/java/com/richardpingree/tournamenttracker/TournamentListActivity.java
@@ -0,0 +1,7 @@
+package com.richardpingree.tournamenttracker;
+
+/**
+ * Created by Richard Pingree APD2 1505 on 5/12/15.
+ */
+public class TournamentListActivity {
+}
