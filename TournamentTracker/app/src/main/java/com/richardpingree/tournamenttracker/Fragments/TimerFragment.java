cd1ab3b16bbcbfa63d12d46722ae6271a5c910fa
richpingree <richpingree@fullsail.edu>
2015-05-22 13:10:31 -0500

    Created CountDownTimer
    Created CountDownTimer, timer works with static data and works as
    intended.  Also made changes to  Blind form layout to center the text
    in the EditText fields

--- TournamentTracker/app/src/main/java/com/richardpingree/tournamenttracker/Fragments/TimerFragment.java
+++ TournamentTracker/app/src/main/java/com/richardpingree/tournamenttracker/Fragments/TimerFragment.java
@@ -3,6 +3,7 @@ package com.richardpingree.tournamenttracker.Fragments;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,10 +13,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.richardpingree.tournamenttracker.BlindListActivity;
 import com.richardpingree.tournamenttracker.R;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by richardpingree on 5/18/15.
@@ -24,9 +28,13 @@ public class TimerFragment extends Fragment {
 
     private final String TAG = "TimerFragment.TAG";
 
+    private CountDownTimer countDownTimer;
+    private final long startTime = 1800 * 1000;
+    private final long interval = 1 * 1000;
     TextView timer, smallBlind, bigBlind;
-
+    ImageButton prevBtn, playBtn, pauseBtn, nextBtn;
     Button resetButton;
+
 
     public TimerFragment(){
 
@@ -44,11 +52,86 @@ public class TimerFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         setHasOptionsMenu(true);
 
+        countDownTimer = new MyCountDownTimer(startTime, interval);
+
         timer = (TextView)getView().findViewById(R.id.timerView);
+        timer.setText(""+String.format("%d:00", TimeUnit.MILLISECONDS.toMinutes(startTime)));
+        prevBtn = (ImageButton)getView().findViewById(R.id.previousBtn);
+        playBtn = (ImageButton)getView().findViewById(R.id.playBtn);
+        pauseBtn = (ImageButton)getView().findViewById(R.id.pauseBtn);
+        nextBtn = (ImageButton)getView().findViewById(R.id.nextBtn);
         smallBlind = (TextView)getView().findViewById(R.id.smallBlindValue);
         bigBlind = (TextView)getView().findViewById(R.id.bigBlindValue);
         resetButton = (Button)getView().findViewById(R.id.resetBtn);
 
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.start();
+
+            }
+        });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.cancel();
+
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.onTick(startTime);
+            }
+        });
+
+    }
+
+    public class MyCountDownTimer extends CountDownTimer{
+
+        /**
+         * @param startTime    The number of millis in the future from the call
+         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+         *                          is called.
+         * @param interval The interval along the way to receive
+         *                          {@link #onTick(long)} callbacks.
+         */
+        public MyCountDownTimer(long startTime, long interval) {
+            super(startTime, interval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            //timer.setText("" + millisUntilFinished/1000);
+            //timer.setText((millisUntilFinished/60000)+ ":" +(millisUntilFinished % 60000/1000));
+//            timer.setText(""+String.format("%d min, %d sec", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+//                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+//                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+            timer.setText(""+String.format("%d:%02d", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+        }
+
+        @Override
+        public void onFinish() {
+
+        }
     }
 
     @Override
